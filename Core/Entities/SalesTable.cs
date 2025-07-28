06b37d19b43d044969791b7f9f340354ca16b361
Ahmed Dewidar <ahmed.mahmoud18498@yahoo.com>
2025-07-28 18:43:35 +0300

    Add SalesTable Entity

---
+++ Core/Entities/SalesTable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class SalesTable
+    {
+        [Key]
+        public string SalesId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public int UpdatedBy { get; set; }
+    }
+}
